ArrowOS/android_packages_apps_Launcher3

Launcher3: Add restart action
Change-Id: I562ef0baead993a0b1e2881a399ea97a98513f3f
Signed-off-by: DennySPb <dennyspb@gmail.com>

src/com/android/launcher3/settings/SettingsActivity.java
@@ -93,7 +93,10 @@ public class SettingsActivity extends FragmentActivity
     }
 
     @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) { }
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (Utilities.KEY_SHOW_SEARCHBAR.equals(key)) {
+                Utilities.restart(this);
+   }
 
     private boolean startFragment(String fragment, Bundle args, String key) {
         if (Utilities.ATLEAST_P && getSupportFragmentManager().isStateSaved()) {

Totozao/packages_apps_Launcher3

src/com/android/launcher3/settings/SettingsActivity.java
@@ -139,7 +139,11 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
     }
 
     @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) { }
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (Utilities.KEY_DT_GESTURE.equals(key)) {
+                Utilities.restart(this);
+        }
+    }
 
     private boolean startPreference(String fragment, Bundle args, String key) {
         if (Utilities.ATLEAST_P && getSupportFragmentManager().isStateSaved()) {